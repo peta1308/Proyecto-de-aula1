e46496a0ca02ccb02fe1fb5c76d8f15c9a3feae8
peta1308 <cuentagithub1308@gmail.com>
2025-05-01 18:15:50 -0500

    Delete Gestion de quejas directory

--- Gestion de quejas/Gestion de quejas/src/Datos.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Datos {
-
-  public static  ArrayList <Usuario> usuarios = new ArrayList<>();
-  public static ArrayList <Reporte> reportes = new ArrayList<>();
-    
-}
